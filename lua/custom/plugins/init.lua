0ad24dec7065c584fff660f9f3bf4ce920f45c6b
Ryan Bitonio <ryanbitonio.ayang@gmail.com>
2025-10-15 16:37:48 +0100

    Initial Neovim config

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,21 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {},
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
+    },
+  },
+  {
+    'numToStr/Comment.nvim',
+    opts = {
+      -- add any options here
+    },
+  },
+}
